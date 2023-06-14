2f9b379e1a728bfaedbc177122499db975bad69e
yahelcohen01 <yahelcohen01@gmail.com>
2023-06-14 16:15:49 +0300

    add random quote card. adviceslip API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Weather from './Weather/Weather';
 
 import axios from 'axios';
 import TodoList from './TodoList/TodoList';
+import Quote from './Quote/Quote';
 import './index.css';
 
 class App extends Component {
@@ -11,18 +12,35 @@ class App extends Component {
 		this.state = {
 			weather: [],
 			temp: [],
-			clouds: []
+			clouds: [],
+			quote: ''
 		}
 	}
+	componentDidMount() {
+		this.fetchQuote();
+	}
 
-	getWeather = query => {
+	fetchQuote = () => {
+		axios.get('https://api.adviceslip.com/advice')
+			.then((response) => {
+				const { advice } = response.data.slip;
+
+				this.setState({ quote: advice });
+			})
+
+			.catch((error) => {
+				console.log(error);
+			})
+	}
+
+	getWeather = () => {
 		axios.get('https://api.openweathermap.org/data/2.5/find?q=jerusalem&units=metric&appid=c4b2e1361fac39b9a24cc90848cddb5d')
 			.then(response => {
 				this.setState({
 					weather: response.data.list[0],
 					temp: response.data.list[0].main.temp,
 					clouds: response.data.list[0].weather[0].description
-				});
+				})
 			})
 			.catch(error => {
 				console.log('Error', error);
@@ -35,18 +53,26 @@ class App extends Component {
 			cityName = event.target.value;
 			this.getWeather(cityName);
 		}
-	}
+	};
 
 	render() {
+
 		return (
-			<div className="container">
+			<div className="App">
 				<header>
 					<h3 className="text-capitalize">weather</h3>
-					<Weather getWeather={this.getWeather('jerusalem')} city={this.state.weather.name} temp={this.state.temp} clouds={this.state.clouds} />
+					<Weather getWeather={this.getWeather} city={this.state.weather.name} temp={this.state.temp} clouds={this.state.clouds} />
+					<div className="card">
+						<h3 className="text-capitalize h3">
+							{this.state.quote}
+						</h3>
+					</div>
 				</header>
-				<div className="row">
-					<TodoList />
-				</div>
+				<main>
+					<div className="row">
+						<TodoList />
+					</div>
+				</main>
 			</div>
 			
 		);
